04e2298d00454c18a15b83401d78ca792269ac50
Jacob McKerrell <jacobmckerrell@gmail.com>
2025-11-01 17:39:42 +0000

    Started on data_collection

---
+++ data_collection.py
@@ -0,0 +1,79 @@
+import requests
+import json
+from datetime import datetime
+import time
+
+BASE_URL = "https://api.elections.kalshi.com/trade-api/v2"
+ENDPOINT = "/markets"
+URL = BASE_URL + ENDPOINT
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+params = {
+    "status": "settled",
+    "limit": 100
+}
+
+"""ticker, title, CATEGORY, YES PRICE, NO PRICE, VOLUME, TIME TO EXPIRATION, CLOSE EARLY?, OPEN INTEREST"""
+
+
+
+class Market:
+
+    def __init__(self, market):
+
+        ticker_str = market.get("ticker")
+        close_time_str = market.get("close_time").split(".")[0]
+        open_time_str = market.get("open_time").split(".")[0]
+        self.settlement_price = market.get("settlement_value")
+        self.title = market.get("title", "N/A")
+        self.volume = market.get("volume")
+
+        #print ("close", close_time_str.rstrip('Z'))
+        #print("open", open_time_str.rstrip('Z'))
+
+
+
+        close_time_dt = datetime.strptime(close_time_str.rstrip('Z'), DATE_FORMAT)
+        self.close_time = close_time_dt.timestamp()
+        open_time_dt = datetime.strptime(open_time_str.rstrip('Z'), DATE_FORMAT)
+        self.open_time = open_time_dt.timestamp()
+        
+        self.settlement_price = market.get("settlement_price", None)
+        
+        self.title = market.get("title", "N/A")
+        
+        parts = ticker_str.split('-', 2)
+
+
+
+        self.series_ticker = parts[0]
+        self.event_ticker = parts[1]
+        self.outcome = parts[2]
+        self.full_ticker = ticker_str
+
+
+
+try:
+    print(f"Attemping to fetch data from {URL}")
+    response = requests.get(URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    market_list = []
+    skipped = 0
+    for market in data.get("markets", []):
+        m = Market(market)
+        market_list.append(m)
+        
+
+    print(f"\n Successfully retrieved {len(market_list)} settled market tickers.")
+    print("skipped:", skipped)
+    if market_list:
+        for i in range(0, 10):
+            print(f"{i+1}.  {(market_list[i]).full_ticker}")
+    
+
+    print(market_list[0].open_time)
+    print(market_list[0].close_time)
+except requests.exceptions.RequestException as e:
+    print(e)
